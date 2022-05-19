0b1536f49d6b5af79de4e9e403ae8cf40fe090a0
Roman Zherebko <romanzherebko@Mac-mini-Roman.local>
2022-05-19 11:39:55 +0300

    tip advisor project

---
+++ TipAdvisor/TipAdvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipAdvisorApp.swift
+//  TipAdvisor
+//
+//  Created by Roman Zherebko on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TipAdvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
